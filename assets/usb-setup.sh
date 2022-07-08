6d5109499bdf9afaf61910cccefeec88474eeaa7
gokul <gokulakams@gmail.com>
2022-07-08 11:26:22 +0530

    changed product id for scarlet

--- assets/usb-setup.sh
+++ assets/usb-setup.sh
@@ -1,7 +1,6 @@
 sudo modprobe cp210x
-echo 0001 0001 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
-echo 0002 0002 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
-echo 0003 0003 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
-echo 0004 0004 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+echo 10c4 0001 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+echo 10c4 0002 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+echo 10c4 0003 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+echo 10c4 0004 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
 echo 10c4 0005 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
-echo 10c4 0006 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
